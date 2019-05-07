4d8bf707dd7a0c93af977689924592fb5068c2af
Vicente J. Ruiz Jurado <vjrj@ourproject.org>
2019-05-08 00:00:10 +0200

    Almost funtional

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,19 +6,19 @@ const defaultStore = false;
 const defUseSubdomain = function(a) {
   return a.LA_collectory_uses_subdomain;
 };
-
+const debug = false;
 const defUseSubdomainPrompt = function(a, service) {
   return `Will the ${service} service use a http://${chalk.red("subdomain")}.${a.LA_domain} or not?`;
 };
 
 module.exports = class extends Generator {
-  prompting() {
+  async prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(`Welcome to the ${chalk.red("Living Atlas Ansible Assistant")} for new LA nodes!`, 15)
     );
 
-    const prompts = [
+    this.answers = await this.prompt([
       {
         store: true,
         type: "input",
@@ -69,7 +69,7 @@ module.exports = class extends Generator {
         store: defaultStore,
         type: "input",
         name: "LA_collectory_hostname",
-        message: "Collectory hostname",
+        message: "LA collectory hostname",
         default: function(a) {
           return a.LA_collectory_uses_subdomain ? `collectory.${a.LA_domain}`: a.LA_domain;
         }
@@ -84,96 +84,79 @@ module.exports = class extends Generator {
           return !a.LA_collectory_uses_subdomain;
         }
       },
-      {
-        store: defaultStore,
-        type: "confirm",
-        name: "LA_ala_hub_uses_subdomain",
-        message: function(a) {
-          return defUseSubdomainPrompt(a, "ala-hub");
-        },
-        default: function(a) { return defUseSubdomain(a); }
-      },
-      {
-        store: defaultStore,
-        type: "input",
-        name: "LA_ala_hub_hostname",
-        message: "LA hub hostname",
-        default: function(a) {
-          return a.LA_ala_hub_uses_subdomain ? `biocache.${a.LA_domain}`: a.LA_domain;
-        }
-      },
-      {
-        store: defaultStore,
-        type: "input",
-        name: "LA_ala_hub_path",
-        default: "/ala-hub",
-        when: function(a) {
-          return !a.LA_ala_hub_uses_subdomain;
-        }
-      }
+      { store: defaultStore, type: "confirm", name: "LA_ala_hub_uses_subdomain", message: function(a) { return defUseSubdomainPrompt(a, "biocache"); }, default: function(a) { return defUseSubdomain(a); } },
+      { store: defaultStore, type: "input", name: "LA_ala_hub_hostname", message: "LA biocache hostname", default: function(a) { return a.LA_ala_hub_uses_subdomain ? `biocache.${a.LA_domain}`: a.LA_domain; } },
+      { store: defaultStore, type: "input", name: "LA_ala_hub_path", default: "/ala-hub", when: function(a) { return !a.LA_ala_hub_uses_subdomain; } },
+      { store: defaultStore, type: "confirm", name: "LA_biocache_service_uses_subdomain", message: function(a) { return defUseSubdomainPrompt(a, "biocache-service"); }, default: function(a) { return defUseSubdomain(a); } },
+      { store: defaultStore, type: "input", name: "LA_biocache_service_hostname", message: "LA biocache-service hostname", default: function(a) { return a.LA_biocache_service_uses_subdomain ? `biocache-ws.${a.LA_domain}`: a.LA_domain; } },
+      { store: defaultStore, type: "input", name: "LA_biocache_service_path", default: "/biocache-service", when: function(a) { return !a.LA_biocache_service_uses_subdomain; } },
 
-        /* { store: defaultStore, type: "", name: "LA_biocache_service_uses_subdomain", message: "", default: "no" },
-         * { store: defaultStore, type: "", name: "LA_biocache_service_hostname", message: "", default: "{{ '%s.%s'|format('biocache-ws' }, answers.LA_domain) if answers.LA_biocache_service_uses_subdomain == 'yes' else '%s'|format(answers.LA_domain) }}",
-         *   { store: defaultStore, type: "", name: "LA_biocache_service_path", message: "", default: "{{ '' if answers.LA_biocache_service_uses_subdomain == 'yes' else '/biocache-service' }}" },
+      { store: defaultStore, type: "confirm", name: "LA_ala_bie_uses_subdomain", message: function(a) { return defUseSubdomainPrompt(a, "bie"); }, default: function(a) { return defUseSubdomain(a); } },
+      { store: defaultStore, type: "input", name: "LA_ala_bie_hostname", message: "LA bie hostname", default: function(a) { return a.LA_ala_bie_uses_subdomain ? `bie.${a.LA_domain}`: a.LA_domain; } },
+      { store: defaultStore, type: "input", name: "LA_ala_bie_path", default: "/ala-bie", when: function(a) { return !a.LA_ala_bie_uses_subdomain; } },
 
-         *   { store: defaultStore, type: "", name: "LA_ala_bie_uses_subdomain", message: "", default: "no" },
-         *   { store: defaultStore, type: "", name: "LA_ala_bie_hostname", message: "", default: "{{ '%s.%s'|format('bie' }, answers.LA_domain) if answers.LA_ala_bie_uses_subdomain == 'yes' else '%s'|format(answers.LA_domain) }}",
-         *     { store: defaultStore, type: "", name: "LA_ala_bie_path", message: "", default: "{{ '' if answers.LA_ala_bie_uses_subdomain == 'yes' else '/ala-bie' }}" },
+      { store: defaultStore, type: "confirm", name: "LA_bie_index_uses_subdomain", message: function(a) { return defUseSubdomainPrompt(a, "bie-service"); }, default: function(a) { return defUseSubdomain(a); } },
+      { store: defaultStore, type: "input", name: "LA_bie_index_hostname", message: "LA bie-service hostname", default: function(a) { return a.LA_bie_index_uses_subdomain ? `bie-ws.${a.LA_domain}`: a.LA_domain; } },
+      { store: defaultStore, type: "input", name: "LA_bie_index_path", default: "/bie-index", when: function(a) { return !a.LA_bie_index_uses_subdomain; } },
 
-         *     { store: defaultStore, type: "", name: "LA_bie_index_uses_subdomain", message: "", default: "no" },
-         *     { store: defaultStore, type: "", name: "LA_bie_index_hostname", message: "", default: "{{ '%s.%s'|format('bie-ws' }, answers.LA_domain) if answers.LA_bie_index_uses_subdomain == 'yes' else '%s'|format(answers.LA_domain) }}",
-         *       { store: defaultStore, type: "", name: "LA_bie_index_path", message: "", default: "{{ '' if answers.LA_bie_index_uses_subdomain == 'yes' else '/bie-index' }}" },
+      { store: defaultStore, type: "confirm", name: "LA_lists_uses_subdomain", message: function(a) { return defUseSubdomainPrompt(a, "specieslists"); }, default: function(a) { return defUseSubdomain(a); } },
+      { store: defaultStore, type: "input", name: "LA_lists_hostname", message: "LA specieslists hostname", default: function(a) { return a.LA_lists_uses_subdomain ? `lists.${a.LA_domain}`: a.LA_domain; } },
+      { store: defaultStore, type: "input", name: "LA_lists_path", default: "/specieslists", when: function(a) { return !a.LA_lists_uses_subdomain; } },
 
-         *       { store: defaultStore, type: "", name: "LA_lists_uses_subdomain", message: "", default: "no" },
-         *       { store: defaultStore, type: "", name: "LA_lists_hostname", message: "", default: "{{ '%s.%s'|format('lists' }, answers.LA_domain) if answers.LA_lists_uses_subdomain == 'yes' else '%s'|format(answers.LA_domain) }}",
-         *         { store: defaultStore, type: "", name: "LA_lists_path", message: "", default: "{{ '' if answers.LA_lists_uses_subdomain == 'yes' else '/specieslists' }}" },
+      { store: defaultStore, type: "confirm", name: "LA_images_uses_subdomain", message: function(a) { return defUseSubdomainPrompt(a, "images"); }, default: function(a) { return defUseSubdomain(a); } },
+      { store: defaultStore, type: "input", name: "LA_images_hostname", message: "LA images hostname", default: function(a) { return a.LA_images_uses_subdomain ? `images.${a.LA_domain}`: a.LA_domain; } },
+      { store: defaultStore, type: "input", name: "LA_images_path", default: "/images", when: function(a) { return !a.LA_images_uses_subdomain; } },
 
-         *         { store: defaultStore, type: "", name: "LA_images_uses_subdomain", message: "", default: "no" },
-         *         { store: defaultStore, type: "", name: "LA_images_hostname", message: "", default: "{{ '%s.%s'|format('images' }, answers.LA_domain) if answers.LA_images_uses_subdomain == 'yes' else '%s'|format(answers.LA_domain) }}",
-         *           { store: defaultStore, type: "", name: "LA_images_path", message: "", default: "{{ '' if answers.LA_images_uses_subdomain == 'yes' else '/images' }}" },
+      { store: defaultStore, type: "confirm", name: "LA_regions_uses_subdomain", message: function(a) { return defUseSubdomainPrompt(a, "regions"); }, default: function(a) { return defUseSubdomain(a); } },
+      { store: defaultStore, type: "input", name: "LA_regions_hostname", message: "LA regions hostname", default: function(a) { return a.LA_regions_uses_subdomain ? `regions.${a.LA_domain}`: a.LA_domain; } },
+      { store: defaultStore, type: "input", name: "LA_regions_path", default: "/regions", when: function(a) { return !a.LA_regions_uses_subdomain; } },
 
-         *           { store: defaultStore, type: "", name: "LA_regions_uses_subdomain", message: "", default: "no" },
-         *           { store: defaultStore, type: "", name: "LA_regions_hostname", message: "", default: "{{ '%s.%s'|format('regions' }, answers.LA_domain) if answers.LA_regions_uses_subdomain == 'yes' else '%s'|format(answers.LA_domain) }}",
-         *             { store: defaultStore, type: "", name: "LA_regions_path", message: "", default: "{{ '' if answers.LA_regions_uses_subdomain == 'yes' else '/regions' }}" },
+      { store: defaultStore, type: "confirm", name: "LA_logger_uses_subdomain", message: function(a) { return defUseSubdomainPrompt(a, "logger"); }, default: function(a) { return defUseSubdomain(a); } },
+      { store: defaultStore, type: "input", name: "LA_logger_hostname", message: "LA logger hostname", default: function(a) { return a.LA_logger_uses_subdomain ? `logger.${a.LA_domain}`: a.LA_domain; } },
+      { store: defaultStore, type: "input", name: "LA_logger_path", default: "/logger", when: function(a) { return !a.LA_logger_uses_subdomain; } },
 
-         *             { store: defaultStore, type: "", name: "LA_logger_uses_subdomain", message: "", default: "no" },
-         *             { store: defaultStore, type: "", name: "LA_logger_hostname", message: "", default: "{{ '%s.%s'|format('logger' }, answers.LA_domain) if answers.LA_logger_uses_subdomain == 'yes' else '%s'|format(answers.LA_domain) }}",
-         *               { store: defaultStore, type: "", name: "LA_logger_path", message: "", default: "{{ '' if answers.LA_logger_uses_subdomain == 'yes' else '/logger-service' }}" },
+      { store: defaultStore, type: "confirm", name: "LA_solr_uses_subdomain", message: function(a) { return defUseSubdomainPrompt(a, "solr"); }, default: function(a) { return defUseSubdomain(a); } },
+      { store: defaultStore, type: "input", name: "LA_solr_hostname", message: "LA solr hostname", default: function(a) { return a.LA_solr_uses_subdomain ? `index.${a.LA_domain}`: a.LA_domain; } },
+      { store: defaultStore, type: "input", name: "LA_solr_path", default: "/solr", when: function(a) { return !a.LA_solr_uses_subdomain; } },
 
-         *               { store: defaultStore, type: "", name: "LA_solr_uses_subdomain", message: "", default: "no" },
-         *               { store: defaultStore, type: "", name: "LA_solr_hostname", message: "", default: "{{ '%s.%s'|format('index' }, answers.LA_domain) if answers.LA_solr_uses_subdomain == 'yes' else '%s'|format(answers.LA_domain) }}",
-         *                 { store: defaultStore, type: "", name: "LA_solr_path", message: "", default: "{{ '' if answers.LA_solr_uses_subdomain == 'yes' else '/solr' }}" },
+      { store: defaultStore, type: "input", name: "LA_cas_hostname", message: "LA CAS hostname", default: function(a) { return `auth.${a.LA_domain}`; }},
 
-         *                 { store: defaultStore, type: "", name: "LA_cas_hostname", message: "", default: "{{ '%s.%s'|format('auth' }, answers.LA_domain) }}",
+      { store: defaultStore, type: "confirm", name: "LA_enable_ssl", message: "Enable SSL? (Warn: this only set the urls correctly, but it doesn't manage the certicates)", default: false },
 
-         *                   { store: defaultStore, type: "", name: "LA_enable_ssl", message: "", default: "no" },
-         *                   { store: defaultStore, type: "", name: "LA_urls_prefix", message: "", default: "{{ 'https://' if answers.LA_enable_ssl == 'yes' else 'http://' }}" },
+      { store: defaultStore, type: "confirm", name: "LA_use_CAS", message: "Use CAS Auth service?", default: false },
+      { store: defaultStore, type: "confirm", name: "LA_use_regions", message: "Use regions service?", default: false },
+      { store: defaultStore, type: "confirm", name: "LA_use_spatial", message: "Use spatial service?", default: false },
+      { store: defaultStore, type: "confirm", name: "LA_use_species_lists", message: "Use specieslists service?", default: false }
 
-         *                   { store: defaultStore, type: "", name: "LA_use_CAS", message: "", default: "no" },
-         *                   { store: defaultStore, type: "", name: "LA_use_regions", message: "", default: "no" },
-         *                   { store: defaultStore, type: "", name: "LA_use_spatial", message: "", default: "no" },
-         *                   { store: defaultStore, type: "", name: "LA_use_species_lists": "no" }
-         */
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-      this.log(JSON.stringify(this.props), null, '\t');
-      if (this.props.LA_collectory_uses_subdomain) { this.props.LA_collectory_path = ""; }
-      if (this.props.LA_ala_hub_uses_subdomain)    { this.props.LA_ala_hub_path = ""; }
-    });
+    ]);
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
+    if (this.answers.LA_collectory_uses_subdomain) { this.answers.LA_collectory_path = ""; }
+    if (this.answers.LA_ala_hub_uses_subdomain)    { this.answers.LA_ala_hub_path = ""; }
+    if (this.answers.LA_biocache_service_uses_subdomain)    { this.answers.LA_biocache_service_path = ""; }
+    if (this.answers.LA_ala_bie_uses_subdomain)    { this.answers.LA_ala_bie_path = ""; }
+    if (this.answers.LA_bie_index_uses_subdomain)    { this.answers.LA_bie_index_path = ""; }
+    if (this.answers.LA_lists_uses_subdomain)    { this.answers.LA_lists_path = ""; }
+    if (this.answers.LA_images_uses_subdomain)    { this.answers.LA_images_path = ""; }
+    if (this.answers.LA_regions_uses_subdomain)    { this.answers.LA_regions_path = ""; }
+    if (this.answers.LA_logger_uses_subdomain)    { this.answers.LA_logger_path = ""; }
+    if (this.answers.LA_solr_uses_subdomain)    { this.answers.LA_solr_path = ""; }
+
+    this.answers.LA_urls_prefix = this.answers.LA_enable_ssl ? 'https://' : 'http://';
+
+    if (debug) this.log(this.answers);
+
+    this.fs.copyTpl(
+      this.templatePath("quick-start-inventory.yml"),
+      this.destinationPath("quick-start-inventory.yml"),
+      this.answers
     );
   }
 
   install() {
-   // this.installDependencies();
-   // TODO: Maybe here we can check for some ansible install dependency
+    // this.installDependencies();
+    // clone ala-install
+    // TODO: Maybe here we can check for some ansible install dependency
   }
 };
